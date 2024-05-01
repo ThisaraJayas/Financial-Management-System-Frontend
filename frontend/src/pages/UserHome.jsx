ccd65c831e24fcbdb390c6ade0bb574d523776bd
Thisara <sasmithajayasinghe1@gmail.com>
2024-05-02 00:52:53 +0530

    user

---
+++ frontend/src/pages/UserHome.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UserHeader from '../components/common/UserHeader'
+
+export default function () {
+  return (
+    <div>
+        <UserHeader/>
+    </div>
+  )
+}
--- frontend/src/routes/RoutesPath.jsx
+++ frontend/src/routes/RoutesPath.jsx
@@ -4,6 +4,7 @@ import Header from "../components/common/Header";
 import Home from "../pages/Home";
 import Register from "../pages/Register";
 import Login from "../pages/Login";
+import UserHome from "../pages/UserHome";
 
 export default function RoutesPath() {
   return (
@@ -12,6 +13,7 @@ export default function RoutesPath() {
         <Route path="/" element={<Home />} />
         <Route path="/register" element={<Register/>}/>
         <Route path="/login" element={<Login/>}/>
+        <Route path="/home" element={<UserHome/>}/>
       </Routes>
     </>
   );
