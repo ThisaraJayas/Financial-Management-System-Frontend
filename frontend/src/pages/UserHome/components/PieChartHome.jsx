4b8a68050675929cc04c64175da0529c872d639e
Thisara <sasmithajayasinghe1@gmail.com>
2024-06-14 15:18:20 +0530

    piech

--- frontend/src/pages/UserHome/components/PieChartHome.jsx
+++ frontend/src/pages/UserHome/components/PieChartHome.jsx
@@ -21,6 +21,7 @@ export default function PieChartHome() {
         }
         fectchTotalIncome()
       },[])
+      
       useEffect(()=>{
         const fectchTotalExpense = async()=>{
           try{
