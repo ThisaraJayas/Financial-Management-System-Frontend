6ef135edd306edb88582635870419981a0a07ebf
Thisara <sasmithajayasinghe1@gmail.com>
2024-06-14 14:52:26 +0530

    transactionpg

--- frontend/src/pages/UserHome/components/TopBoxes.jsx
+++ frontend/src/pages/UserHome/components/TopBoxes.jsx
@@ -14,16 +14,28 @@ import { UserContext } from "../../../context/UserContext";
 export default function TopBoxes() {
     const {userData} = useContext(UserContext)
     const userId=userData.id
-    const [currentBalance, setCurrentBalance] = useState(0)
+    const [totalIncome, setTotalIncome] = useState(0)
     const [currentLiabilities, setCurrentLiabilities]=useState(0)
-    const [monthlyIncome, setMonthlyIncome]=useState(0)
+    const [totalExpenses, setTotalExpenses]=useState(0)
+    const [balance, setBalance]=useState(0)
+
+    useEffect(() => {
+      const capital = totalIncome - totalExpenses;
+      if (capital < 0) {
+          setCurrentLiabilities(capital);
+          setBalance(0);
+      } else {
+          setBalance(capital);
+          setCurrentLiabilities(0);
+      }
+  }, [totalIncome, totalExpenses]);
   
     useEffect(()=>{
       const fectchTotalIncome = async()=>{
         try{
-          const response = await axios.get(`http://localhost:8061/users/${userId}/balances/currentbalances`)
+          const response = await axios.get(`http://localhost:8061/users/${userId}/incomes/currentbalance`)
           console.log(response);
-          setCurrentBalance(response.data)
+          setTotalIncome(response.data)
         }catch(error){
           console.log(error);
         }
@@ -31,16 +43,16 @@ export default function TopBoxes() {
       fectchTotalIncome()
     },[])
     useEffect(()=>{
-      const fectchMonthlyIncome = async()=>{
+      const fectchTotalIncome = async()=>{
         try{
-          const response = await axios.get(`http://localhost:8061/users/${userId}/incomes/monthlyincome`)
+          const response = await axios.get(`http://localhost:8061/users/${userId}/expenses/currentexpenses`)
           console.log(response);
-          setMonthlyIncome(response.data)
+          setTotalExpenses(response.data)
         }catch(error){
           console.log(error);
         }
       }
-      fectchMonthlyIncome()
+      fectchTotalIncome()
     },[])
   return (
     <div className="container pt-8 pl-8">
@@ -48,7 +60,36 @@ export default function TopBoxes() {
             <h3>Income Dashboard</h3>
           </div>
           <div className="mainCards">
+          <div className="cards flex justify-center">
+              <div className="flex justify-center">
+                <GiTakeMyMoney
+                  className="icons text-blue-500"
+                  style={{ fontSize: 55 }}
+                />
+              </div>
+              <div className="cardHead flex justify-center">
+                <h2 class="text-xl font-semibold text-gray-800 whitespace-nowrap">
+                 Balance
+                </h2>
+              </div>
+              <h1 className="text-blue-500">LKR <br></br>{balance}</h1>
+            </div>
+          <div className="cards flex justify-center">
+              <div className="flex justify-center">
+                <GiTakeMyMoney
+                  className="icons text-blue-500"
+                  style={{ fontSize: 55 }}
+                />
+              </div>
+              <div className="cardHead flex justify-center">
+                <h2 class="text-xl font-semibold text-gray-800 whitespace-nowrap">
+                 Liabilities
+                </h2>
+              </div>
+              <h1 className="text-blue-500">LKR <br></br>{currentLiabilities}</h1>
+            </div>
             <div className="cards flex justify-center">
+              
               <div className="flex justify-center">
                 <MonetizationOnIcon
                   className="icons text-green-400"
@@ -57,25 +98,12 @@ export default function TopBoxes() {
               </div>
               <div className="cardHead flex justify-center">
                 <h2 class="text-xl font-semibold text-gray-800 whitespace-nowrap">
-                Current Balance
+                Total income
                 </h2>
               </div>
-              <h1 className="text-green-400">LKR {currentBalance}</h1>
+              <h1 className="text-green-400">LKR <br></br>{totalIncome}</h1>
             </div>
-            {/* <div className="cards flex justify-center">
-              <div className="flex justify-center">
-                <GiReceiveMoney
-                  className="icons text-orange-400"
-                  style={{ fontSize: 55 }}
-                />
-              </div>
-              <div className="cardHead flex justify-center">
-                <h2 class="text-xl font-semibold text-gray-800 whitespace-nowrap">
-                Liabilities
-                </h2>
-              </div>
-              <h1 className="text-orange-400">LKR 200,500</h1>
-            </div> */}
+            
             <div className="cards flex justify-center">
               <div className="flex justify-center">
                 <GiTakeMyMoney
@@ -85,25 +113,12 @@ export default function TopBoxes() {
               </div>
               <div className="cardHead flex justify-center">
                 <h2 class="text-xl font-semibold text-gray-800 whitespace-nowrap">
-                  Monthly Income
+                 Total Expenses
                 </h2>
               </div>
-              <h1 className="text-blue-500">LKR {monthlyIncome}</h1>
+              <h1 className="text-blue-500">LKR<br></br> {totalExpenses}</h1>
             </div>
-            {/* <div className="cards flex justify-center">
-              <div className="flex justify-center">
-                <ShoppingCartIcon
-                  className="icons text-green-400"
-                  style={{ fontSize: 55 }}
-                />
-              </div>
-              <div className="cardHead flex justify-center">
-                <h2 class="text-xl font-semibold text-gray-800 whitespace-nowrap">
-                  Monthly Expense
-                </h2>
-              </div>
-              <h1 className="text-green-400">LKR 200,500</h1>
-            </div> */}
+           
           </div>
 
           <div className="flex justify-between mb-5"></div>
